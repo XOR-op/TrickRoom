2be346b39b8ca3e7b4d1ae7952e483d0278393e8
XOR-op <17672363+XOR-op@users.noreply.github.com>
2021-03-06 22:02:42 +0800

    add codegen.bash

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set ff=UNIX
 set -e
 mkdir -p "out"
-find ./src -name "*.java"|javac -d out -cp /ulib/java/antlr-4.8-complete.jar:. @/dev/stdin
+find ./src -name "*.java"|javac -d out -cp /ulib/java/antlr-4.9.1-complete.jar:. @/dev/stdin
--- semantic.bash
+++ semantic.bash
@@ -1,3 +1,3 @@
 set ff=UNIX
 set -e
-cat |java -cp /ulib/java/antlr-4.8-complete.jar:./out TrickRoom -fsyntax-only
+cat |java -cp /ulib/java/antlr-4.9.1-complete.jar:./out TrickRoom -fsyntax-only
