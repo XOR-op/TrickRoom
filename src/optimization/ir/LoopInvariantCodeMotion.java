7c54420afc5eb36a9a0efa886735313673058796
XOR-op <17672363+XOR-op@users.noreply.github.com>
2021-04-11 13:56:51 +0800

    licm

--- src/optimization/ir/LoopInvariantCodeMotion.java
+++ src/optimization/ir/LoopInvariantCodeMotion.java
@@ -4,10 +4,8 @@ import ir.IRBlock;
 import ir.IRFunction;
 import ir.construct.DominanceTracker;
 import ir.construct.RegisterTracker;
-import ir.instruction.Branch;
-import ir.instruction.IRDestedInst;
-import ir.instruction.IRInst;
-import ir.instruction.Jump;
+import ir.instruction.*;
+import ir.operand.Register;
 import misc.pass.IRFunctionPass;
 
 import java.util.HashMap;
@@ -21,7 +19,6 @@ public class LoopInvariantCodeMotion extends LoopAnalyzer {
 
     public LoopInvariantCodeMotion(IRFunction f) {
         super(f);
-
     }
 
     @Override
@@ -43,7 +40,7 @@ public class LoopInvariantCodeMotion extends LoopAnalyzer {
     }
 
     private void findAndMotion(Loop loop, Loop parentLoop) {
-        var motionInstSet = new HashSet<IRInst>();
+        var motionInstSet = new LinkedList<IRInst>();
         Consumer<IRBlock> perBlockLambda = irBlock -> {
             for (var iter = irBlock.insts.iterator(); iter.hasNext(); ) {
                 var irInst = iter.next();
@@ -80,36 +77,67 @@ public class LoopInvariantCodeMotion extends LoopAnalyzer {
             }
         }
         if (!motionInstSet.isEmpty()) {
-            // add pre-header and perform code motion
-            var preheader = new IRBlock(loop.header.blockName.substring(2) + "PH");
-            var loopHeader = loop.header;
-            if (irFunc.entryBlock == loopHeader) irFunc.entryBlock = preheader;
-            preheader.phiCollection = loopHeader.phiCollection;
-            preheader.prevs = loopHeader.prevs;
-            loopHeader.phiCollection = new HashSet<>();
-            loopHeader.prevs = new HashSet<>();
-            preheader.setJumpTerminator(loopHeader);
-            preheader.prevs.forEach(prevBlock -> {
-                prevBlock.nexts.remove(loopHeader);
-                prevBlock.nexts.add(preheader);
-                if (prevBlock.terminatorInst instanceof Jump) {
-                    ((Jump) prevBlock.terminatorInst).target = preheader;
-                } else {
-                    assert prevBlock.terminatorInst instanceof Branch;
-                    var br = (Branch) prevBlock.terminatorInst;
-                    if (br.trueBranch == loopHeader) br.trueBranch = preheader;
-                    else {
-                        assert br.falseBranch == loopHeader;
-                        br.falseBranch = preheader;
-                    }
-                }
-            });
-            preheader.insts.addAll(motionInstSet);
-            irFunc.addBlock(preheader);
-            preheader.insts.forEach(inst -> usageTracker.updateInstBelonging(inst, preheader));
-            if (parentLoop != null)
-                parentLoop.loopBlock.add(preheader);
+            motion(motionInstSet, loop, parentLoop);
         }
+    }
+
+    private void motion(LinkedList<IRInst> motionInstSet, Loop loop, Loop parentLoop) {
+        // add pre-header and perform code motion
+        var preheader = new IRBlock(loop.header.blockName.substring(2) + "PH");
+        var loopHeader = loop.header;
+        if (irFunc.entryBlock == loopHeader) irFunc.entryBlock = preheader;
+        // block relation
+        preheader.prevs = loopHeader.prevs;
+        preheader.prevs.remove(loop.backStart);
+        loopHeader.prevs = new HashSet<>();
+        loopHeader.prevs.add(loop.backStart);
+        preheader.setJumpTerminator(loopHeader);
+        preheader.prevs.forEach(prevBlock -> {
+            prevBlock.nexts.remove(loopHeader);
+            prevBlock.nexts.add(preheader);
+            if (prevBlock.terminatorInst instanceof Jump) {
+                ((Jump) prevBlock.terminatorInst).target = preheader;
+            } else {
+                assert prevBlock.terminatorInst instanceof Branch;
+                var br = (Branch) prevBlock.terminatorInst;
+                if (br.trueBranch == loopHeader) br.trueBranch = preheader;
+                else {
+                    assert br.falseBranch == loopHeader;
+                    br.falseBranch = preheader;
+                }
+            }
+        });
+
+        // deal with phi
+        preheader.phiCollection = loopHeader.phiCollection;
+        loopHeader.phiCollection = new HashSet<>();
+        for (var phiIter = preheader.phiCollection.iterator(); phiIter.hasNext(); ) {
+            var phi = phiIter.next();
+            for (var iter = phi.arguments.iterator(); iter.hasNext(); ) {
+                var ele = iter.next();
+                if (ele.block == loop.backStart) {
+                    var oldDest = phi.dest;
+                    var newPhi = new Phi(oldDest);
+                    phi.dest = new Register(oldDest.type, "t_" + oldDest.identifier());
+                    newPhi.append(phi.dest, preheader);
+                    newPhi.append(ele.val, loop.backStart);
+                    loopHeader.phiCollection.add(newPhi);
+                    iter.remove();
+                    break;
+                }
+            }
+            if (phi.arguments.size() == 1) {
+                preheader.insertInstFromHead(new Assign(phi.dest, phi.arguments.get(0).val));
+                phiIter.remove();
+            }
+        }
+
+        // add insts
+        preheader.insts.addAll(motionInstSet);
+        irFunc.addBlock(preheader);
+        preheader.insts.forEach(inst -> usageTracker.updateInstBelonging(inst, preheader));
+        if (parentLoop != null)
+            parentLoop.loopBlock.add(preheader);
     }
 
 }
